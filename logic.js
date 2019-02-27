1b95a6f922fce8c87900ee9b5c922f90bffca3f9
Cody <cody.tracy92@gmail.com>
2019-02-27 16:43:51 -0600

    added functions to get gifs on page and ajax request

--- logic.js
+++ logic.js
@@ -10,9 +10,53 @@ function displayGifs (){
     })
     .then(function(response){
         console.log(response);
+        var results = response.data;
+
+        for (var i =0; i <results.length; i++){
+            var gifDiv = $("<div>");
+
+            var rating = results[i].rating;
+
+            var p = $("<p>").text("Rating: " + rating);
+
+            var gifImage = $("<img>");
+
+            gifImage.attr("src" , results[i].images.fixed_height.url);
+
+            gifDiv.prepend(p);
+            gifDiv.prepend(gifImage);
+
+            $("#gifs-view").prepend(gifDiv);
+        }
+
+
     })
 }
 
 function renderButtons(){
-    
+
+    $("#buttons-view").empty();   
+
+
+    for (var i = 0; i < topics.length; i++){
+        var gifButtons = $("<button>");
+        gifButtons.addClass("gifs");
+        gifButtons.attr("data-name", topics[i]);
+        gifButtons.text(topics[i]);
+        $("#buttons-view").append(gifButtons);
+    }
 }
+
+$("#add-gifs").on("click", function (event){
+    event.preventDefault();
+
+    var gif = $("#gifs-input").val().trim();
+
+    topics.push(gif);
+
+    renderButtons();
+})
+
+$(document).on("click", ".gifs", displayGifs);
+
+renderButtons();
